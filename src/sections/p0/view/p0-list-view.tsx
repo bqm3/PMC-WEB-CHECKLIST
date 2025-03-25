560cfa4afe2c75e626494e80b806c909118549db
manh2903 <manhdz22pl@gmail.com>
2025-03-25 14:38:44 +0700

    add export s0

--- src/sections/p0/view/p0-list-view.tsx
+++ src/sections/p0/view/p0-list-view.tsx
@@ -5,7 +5,13 @@ import Table from '@mui/material/Table';
 import Container from '@mui/material/Container';
 import TableBody from '@mui/material/TableBody';
 import TableContainer from '@mui/material/TableContainer';
-import { Stack } from '@mui/material';
+import Button from '@mui/material/Button';
+import { Stack, Box, CircularProgress } from '@mui/material';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 // routes
 import { paths } from 'src/routes/paths';
 import { useRouter } from 'src/routes/hooks';
@@ -27,6 +33,7 @@ import {
 // types
 import { IChecklistTableFilters, IP0 } from 'src/types/khuvuc';
 //
+import axios from 'axios';
 import P0TableRow from '../p0-table-row';
 
 // ----------------------------------------------------------------------
@@ -50,8 +57,10 @@ const defaultFilters: IChecklistTableFilters = {
 };
 
 // ----------------------------------------------------------------------
+const STORAGE_KEY = 'accessToken';
 
 export default function GiamsatListView() {
+  const accessToken = localStorage.getItem(STORAGE_KEY);
   const table = useTable({ defaultOrderBy: 'Ngay_ghi_nhan' });
 
   const settings = useSettingsContext();
@@ -61,6 +70,12 @@ export default function GiamsatListView() {
 
   const { page, rowsPerPage } = table;
   const { p0, p0Count } = useGetP0_ByDuan(page, rowsPerPage);
+  const [cardDialogOpen, setCardDialogOpen] = useState(false);
+  const [loadingReport, setLoadingReport] = useState<any>();
+  const [dataChecklistMonth, setDataChecklistMonth] = useState<any>({
+    month: null,
+    year: null,
+  });
 
   const [tableData, setTableData] = useState<IP0[]>([]);
 
@@ -69,8 +84,6 @@ export default function GiamsatListView() {
       setTableData(p0);
     }
   }, [p0, page, rowsPerPage]);
-
-
 
   const dataFiltered = applyFilter({
     inputData: tableData,
@@ -91,6 +104,81 @@ export default function GiamsatListView() {
     [router]
   );
 
+  const handleOpenCardDialog = () => {
+    setCardDialogOpen(true);
+  };
+
+  const handleCloseCardDialog = () => {
+    setCardDialogOpen(false);
+  };
+
+  const handleMonthChange = (value: Date | null) => {
+    if (value) {
+      setDataChecklistMonth((prev: any) => ({
+        ...prev,
+        month: value.getMonth() + 1, // Months are 0-indexed in JavaScript
+      }));
+    }
+  };
+
+  const handleYearChange = (value: Date | null) => {
+    // Check if the selected date is valid and update the year
+    if (value) {
+      setDataChecklistMonth((prev: any) => ({
+        ...prev,
+        year: value.getFullYear(), // Get the year from the date
+      }));
+    }
+  };
+
+  const fetchExcelData = async () => {
+    try {
+      setLoadingReport(true);
+      const response = await axios.get(
+        `https://checklist.pmcweb.vn/be/api/v2/p0/export-excel/?year=${dataChecklistMonth.year}&month=${dataChecklistMonth.month}`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+          },
+          responseType: 'blob',
+        }
+      );
+  
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+
+      // Create a link element
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute(
+        'download',
+        `Bao_cao_s0_${dataChecklistMonth.month}_${dataChecklistMonth.year}.xlsx`
+      ); // Set the file name
+
+      // Append to body and trigger the download
+      document.body.appendChild(link);
+      link.click();
+
+      // Cleanup
+      link.remove(); // Prefer link.remove() over parentNode to directly remove the element
+
+      // Release the blob URL after downloading
+      window.URL.revokeObjectURL(url);
+  
+      setCardDialogOpen(false);
+      setLoadingReport(false);
+    } catch (error) {
+      console.error('Error downloading the file:', error);
+      
+      // Optional: Add error handling to show a user-friendly message
+      // For example, you might want to use a toast or notification library
+      // toast.error('Failed to download the report. Please try again.');
+      
+      setCardDialogOpen(false);
+      setLoadingReport(false);
+    }
+  };
+
   return (
     <Container maxWidth={settings.themeStretch ? false : 'xl'}>
       <Stack direction="row" alignItems="center" justifyContent="space-between">
@@ -107,10 +195,14 @@ export default function GiamsatListView() {
             mb: { xs: 3, md: 5 },
           }}
         />
+        <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '10px' }}>
+          <Button variant="contained" size="large" color="primary" onClick={handleOpenCardDialog}>
+            Báo cáo S0
+          </Button>
+        </div>
       </Stack>
 
       <Card>
-
         <TableContainer sx={{ position: 'relative', overflow: 'unset' }}>
           <Scrollbar>
             <Table size={table.dense ? 'small' : 'medium'} sx={{ minWidth: 960 }}>
@@ -149,6 +241,40 @@ export default function GiamsatListView() {
           </Scrollbar>
         </TableContainer>
 
+        <Dialog open={cardDialogOpen} onClose={handleCloseCardDialog} fullWidth maxWidth="sm">
+          <DialogTitle>Báo cáo S0</DialogTitle>
+          <DialogContent>
+            <Box display="flex" justifyContent="space-between" alignItems="center" gap={1} m={1}>
+              <DatePicker
+                label="Tháng" // Corrected
+                openTo="month"
+                views={['month']}
+                value={
+                  dataChecklistMonth.month ? new Date(2024, dataChecklistMonth.month - 1) : null
+                }
+                onChange={handleMonthChange}
+              />
+              <DatePicker
+                label="Năm" // Corrected
+                views={['year']}
+                value={dataChecklistMonth.year ? new Date(dataChecklistMonth.year, 0) : null}
+                onChange={handleYearChange}
+              />
+            </Box>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseCardDialog}>Close</Button>
+            <Button
+              color="success"
+              variant="contained"
+              disabled={loadingReport}
+              onClick={fetchExcelData}
+            >
+              {loadingReport ? <CircularProgress size={24} color="inherit" /> : 'Xuất file'}
+            </Button>
+          </DialogActions>
+        </Dialog>
+
         <TablePaginationCustom
           count={p0Count}
           page={table.page}
@@ -175,7 +301,7 @@ function applyFilter({
   filters: IChecklistTableFilters;
   // dateError: boolean;
 }) {
-  const { name } = filters
+  const { name } = filters;
   const stabilizedThis = inputData?.map((el, index) => [el, index] as const);
 
   stabilizedThis.sort((a, b) => {
